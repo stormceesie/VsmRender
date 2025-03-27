d702107e01a08ce3e83562d121a2d7ae7c07d74b
f.kegler <f.kegler@voortman.net>
2025-03-27 14:03:31 +0100

    Bat file compileerd eerst alle projecten en kopieert ze tot slot naar de output folder.

--- CreateOutput.bat
+++ CreateOutput.bat
@@ -1,11 +1,37 @@
 @echo off
 
-set SourceFiles=Afstudeerverslag\Afstudeerverslag.pdf LiteratuurOnderzoek\LiteratuurOnderzoek.pdf SystemRequirementDocument\TestkastSRD.pdf FunctioneelOntwerp\FunctioneelOntwerp.pdf
-
+set SourceFiles=Afstudeerverslag\Afstudeerverslag LiteratuurOnderzoek\LiteratuurOnderzoek SystemRequirementDocument\TestkastSRD FunctioneelOntwerp\FunctioneelOntwerp TechnischOntwerp\TechnischOntwerp Handleiding\Handleiding
 set DestFolder=Output
 
+rem Bewaar de huidige directory om later naar terug te keren
+set CurrentDir=%CD%
+
+rem Eerst de bestanden bouwen (pdflatex uitvoeren)
 for %%f in (%SourceFiles%) do (
-	copy "%%f" "%DestFolder%"
+    rem Ga naar de map waar de .tex file zich bevindt
+    cd /d "%%~dpf"
+
+    echo Eerst een keertje pdflatex
+    pdflatex -synctex=1 -interaction=batchmode "%%~nxf".tex
+
+    echo Dan even bibtex compilen
+    bibtex "%%~nxf"
+
+    echo Glossaries compilen e.g. gebruikte acroniemen
+    makeglossaries "%%~nxf"
+
+    echo Tot slot nog 2x pdflatex compilen
+    pdflatex -synctex=1 -interaction=batchmode "%%~nxf".tex
+    pdflatex -synctex=1 -interaction=batchmode "%%~nxf".tex
+
+    rem Keer terug naar de oorspronkelijke directory
+    cd /d "%CurrentDir%"
+)
+
+rem Nu de gegenereerde PDF's kopiëren
+for %%f in (%SourceFiles%) do (
+    echo %%f.pdf kopieren naar %DestFolder%
+    copy "%%f.pdf" "%DestFolder%"
 )
 
 pause
