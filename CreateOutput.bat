df1e7b79c8f53ece1ffb58c0729e2dc46a4d392a
f.kegler <f.kegler@voortman.net>
2025-03-27 14:43:36 +0100

    Handleiding en eisen opgesplitst in aparte files zodat ze weer gebruikt kunnen worden

--- CreateOutput.bat
+++ CreateOutput.bat
@@ -1,5 +1,7 @@
 @echo off
 
+color a
+
 set SourceFiles=Afstudeerverslag\Afstudeerverslag LiteratuurOnderzoek\LiteratuurOnderzoek SystemRequirementDocument\TestkastSRD FunctioneelOntwerp\FunctioneelOntwerp TechnischOntwerp\TechnischOntwerp Handleiding\Handleiding
 set DestFolder=Output
 
@@ -11,16 +13,32 @@ for %%f in (%SourceFiles%) do (
     rem Ga naar de map waar de .tex file zich bevindt
     cd /d "%%~dpf"
 
+    echo ############################################################################
+    echo
     echo Eerst een keertje pdflatex
+    echo
+    echo ############################################################################
     pdflatex -synctex=1 -interaction=batchmode "%%~nxf".tex
 
+    echo ############################################################################
+    echo
     echo Dan even bibtex compilen
+    echo
+    echo ############################################################################
     bibtex "%%~nxf"
 
+    echo ############################################################################
+    echo
     echo Glossaries compilen e.g. gebruikte acroniemen
+    echo
+    echo ############################################################################
     makeglossaries "%%~nxf"
 
+    echo ############################################################################
+    echo
     echo Tot slot nog 2x pdflatex compilen
+    echo
+    echo ############################################################################
     pdflatex -synctex=1 -interaction=batchmode "%%~nxf".tex
     pdflatex -synctex=1 -interaction=batchmode "%%~nxf".tex
 
@@ -28,7 +46,11 @@ for %%f in (%SourceFiles%) do (
     cd /d "%CurrentDir%"
 )
 
-rem Nu de gegenereerde PDF's kopiëren
+echo ###############################################################################
+echo
+echo De .pdf bestanden kopieren naar de output folder
+echo
+echo ###############################################################################
 for %%f in (%SourceFiles%) do (
     echo %%f.pdf kopieren naar %DestFolder%
     copy "%%f.pdf" "%DestFolder%"
---
+++ Readme.txt
@@ -0,0 +1,9 @@
+Dit zijn alle afstudeer documenten van de afstudeerperiode kwartiel 3 en 4 van studiejaar 2024/2025.
+
+Om alle documenten te bouwen met pdflatex, bibtex en makeglossaties kan simpelweg de CreateOutput.bat gestart worden mits de genoemde programmas geinstalleerd zijn op de pc.
+
+Er is bij het maken van de documenten gedacht aan herbruikbaarheid daarom zijn generieke onderdelen gedefinieërd in de GeneralTeX folder zodat bijvoorbeeld de opmaak in elk document altijd gelijk is. Dit zorgt voor consistentie en ook een stukje automatisering.
+
+Dus:
+
+Dubbelclick: CreateOutput.bat
